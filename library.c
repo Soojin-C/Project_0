bbcb1deff3ae7ac272068cf87cb070699dd9bec4
Soojin-C <schoi4@stuy.edu>
2018-10-18 11:02:20 -0400

    classwork edit

--- library.c
+++ library.c
@@ -3,3 +3,13 @@
 #include "linkedList.c"
 #include "library.h"
 
+void print_library(struct song_node * lib){
+
+  while(lib){
+
+   
+
+  }
+
+}
+
--- library.h
+++ library.h
@@ -1,1 +1,17 @@
 struct song_node * table[27];
+
+void print_library();
+
+void print_letter();
+
+void find_song();
+
+void find_artist();
+
+void remove_song();
+
+void clear_library();
+
+void print_artist();
+
+void shuffle();
